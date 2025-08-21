ad6b4ac83339dff38d46d322cd8bb43462e97ae4
skverma771 <60751164+skverma771@users.noreply.github.com>
2025-08-21 11:39:35 +0530

    Add src/controllers/telemetryController.js 1755756574937

---
+++ src/controllers/telemetryController.js
@@ -0,0 +1,16 @@
+const Telemetry = require('../models/Telemetry'); // Assuming a Telemetry model is defined
+
+// Post telemetry data
+exports.postTelemetry = async (req, res) => {
+    // Logic to handle telemetry data here
+};
+
+// Get the latest telemetry for a bike
+exports.getLatestTelemetry = async (req, res) => {
+    // Logic to get latest telemetry here
+};
+
+// Get telemetry data by time range
+exports.getTelemetryByTimeRange = async (req, res) => {
+    // Logic to get telemetry by time range here
+};
