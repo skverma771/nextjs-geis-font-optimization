221a445a63e2b5ec2cd2d7a6f17e8bfb9944ad02
skverma771 <60751164+skverma771@users.noreply.github.com>
2025-08-21 11:39:32 +0530

    Add src/controllers/adminController.js 1755756572388

---
+++ src/controllers/adminController.js
@@ -0,0 +1,22 @@
+const User = require('../models/User'); // Assuming a User model is defined
+const Bike = require('../models/Bike'); // Assuming a Bike model is defined
+
+// Get all users (admin only)
+exports.getUsers = async (req, res) => {
+    // Logic to get all users here
+};
+
+// Get all bikes and their status
+exports.getBikes = async (req, res) => {
+    // Logic to get all bikes here
+};
+
+// Generate usage report
+exports.generateUsageReport = async (req, res) => {
+    // Logic to generate usage report here
+};
+
+// Assign subscription to a user
+exports.assignSubscription = async (req, res) => {
+    // Logic to assign subscription here
+};
